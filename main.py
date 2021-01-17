ebca7beb81cc7a4e291339fe28cf14ae25b3762f
Cezar Voicu <cezarvoicu18@gmail.com>
2021-01-17 17:44:03 +0200

    added about route

--- main.py
+++ main.py
@@ -10,5 +10,12 @@ def home():
 def test(name):
     return f"Welcome to our website, {name}!"
 
+@app.route('/about')
+def about():
+	return  "This is the about page"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
